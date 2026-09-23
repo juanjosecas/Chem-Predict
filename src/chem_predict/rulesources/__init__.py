88792a986b12e6e3dd40992168b8f68c7a2bd1d3
Juan Casal <juan.5ht@gmail.com>
2026-09-23 10:09:07 -0300

    rulesources: add __init__.py

---
+++ src/chem_predict/rulesources/__init__.py
@@ -0,0 +1,54 @@
+from chem_predict.rulesources.chet import CHET_GUIDE_URL, parse_chet_export_csv
+from chem_predict.rulesources.external import EnviPathSource
+from chem_predict.rulesources.models import (
+    AccessMode,
+    ReactionRecord,
+    ReactionRuleRecord,
+    RuleSourceDescriptor,
+    SourceFetchError,
+    SourceKind,
+)
+from chem_predict.rulesources.registry import SOURCES, get_source, list_sources
+from chem_predict.rulesources.retrorules import (
+    RETRO_RULES_VERSION,
+    RetroRulesSource,
+    parse_template_row,
+    parse_templates_tsv,
+)
+from chem_predict.rulesources.rhea import (
+    RHEA_LICENSE_URL,
+    RHEA_REACTION_SMILES_URL,
+    RheaSource,
+    parse_reaction_smiles_lines,
+)
+from chem_predict.rulesources.zenodo import (
+    NORMAN_REFTPS_CONCEPT_DOI,
+    NormanRefTPSSource,
+    ZenodoRecordSource,
+)
+
+__all__ = [
+    "AccessMode",
+    "CHET_GUIDE_URL",
+    "EnviPathSource",
+    "NORMAN_REFTPS_CONCEPT_DOI",
+    "RETRO_RULES_VERSION",
+    "RHEA_LICENSE_URL",
+    "RHEA_REACTION_SMILES_URL",
+    "ReactionRecord",
+    "ReactionRuleRecord",
+    "RetroRulesSource",
+    "RheaSource",
+    "RuleSourceDescriptor",
+    "SOURCES",
+    "SourceFetchError",
+    "SourceKind",
+    "ZenodoRecordSource",
+    "NormanRefTPSSource",
+    "get_source",
+    "list_sources",
+    "parse_chet_export_csv",
+    "parse_reaction_smiles_lines",
+    "parse_template_row",
+    "parse_templates_tsv",
+]
