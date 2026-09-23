faf30302673ecb186f620d24e6e18da23ba7e17d
Juan Casal <juan.5ht@gmail.com>
2026-09-23 12:32:44 -0300

    Document and export mixture prediction

--- src/chem_predict/degradation/__init__.py
+++ src/chem_predict/degradation/__init__.py
@@ -1,3 +1,8 @@
 from chem_predict.degradation.engine import DegradationEngine
+from chem_predict.degradation.mixture import (
+    MixtureComponent,
+    MixtureDegradationEngine,
+    MixturePrediction,
+)
 
-__all__ = ["DegradationEngine"]
+__all__ = ["DegradationEngine", "MixtureComponent", "MixtureDegradationEngine", "MixturePrediction"]
