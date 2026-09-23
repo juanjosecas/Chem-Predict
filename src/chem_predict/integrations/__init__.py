43e125600399c5f987c1afcfef1390684e8f7e74
Juan Casal <juan.5ht@gmail.com>
2026-09-23 09:30:32 -0300

    integrations: add __init__.py

---
+++ src/chem_predict/integrations/__init__.py
@@ -0,0 +1,15 @@
+from chem_predict.integrations.synkit import (
+    BondChange,
+    SynKitITSResult,
+    SynKitUnavailableError,
+    installed_synkit_version,
+    reaction_to_its,
+)
+
+__all__ = [
+    "BondChange",
+    "SynKitITSResult",
+    "SynKitUnavailableError",
+    "installed_synkit_version",
+    "reaction_to_its",
+]
